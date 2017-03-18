38ab094a907fe1b13bc6f3b40074112188580afc
Zack <SimplyComplexable@gmail.com>
2017-03-18 12:57:50 -0600

    first commit

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+
+import {RecipesComponent} from "./recipes.component";
+import {RecipeListComponent} from "./recipe-list/recipe-list.component";
+import {RecipeItemComponent} from "./recipe-item.component";
+import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
+import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
+import {RecipeStartComponent} from "./recipe-start.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {recipesRouting} from "./recipes.routing";
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeEditComponent,
+    RecipeStartComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    recipesRouting
+  ]
+})
+export class RecipesModule { }
